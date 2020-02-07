e16a7dcd4faa4133750800d73c93ab64c41d302d
SolarView <joaovictordecastroa@gmail.com>
2020-02-07 15:21:12 -0500

    Tratamento da resposta da requisição de repositório

--- controllers/repositoryOrdenation.js
+++ controllers/repositoryOrdenation.js
@@ -1,10 +1,28 @@
 const models = require('../models');
 
-async function repositoryOrdanation(username) {
-   // return JSON.stringify(await models.getRepository(username));
-   return (JSON.stringify(await models.getRepository("joaovictordecastroa")));
+
+async function repositoryData(username) {
+   const repositoryJSON = await models.getRepository(username);
+   const repositoryInfo = repositoryJSON.map(repository => {
+      return {
+         name: repository.name ? repository.name : "",
+         url: repository.html_url ? repository.html_url : "",
+         description: repository.description ? repository.description: "Esse repositório não possui uma descrição",
+         apiUrl: repository.url ? repository.url : "",
+         creationDate: repository.created_at ? repository.created_at : "",
+         lastModification: repository.updated_at ? repository.updated_at : "",
+         language: repository.language ? repository.language : "",
+         login: repository.owner.login ? repository.owner.login : "",
+         gitCloneUrl: repository.clone_url,
+         size: repository.size ? repository.size : 0,
+         watchers_count: repository.watchers_count ? repository.watchers_count : 0,
+         topics: repository.topics ? repository.topics : [""]
+      }
+   });
+   return repositoryInfo;
 }
 
-module.exports = repositoryOrdanation;
+
+module.exports = repositoryData;
 
 
