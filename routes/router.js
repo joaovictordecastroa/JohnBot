1e803145b85a1b46b0dd9caa91e469b588d13bdf
joaovictordecastroa <joaovictordecastroa@gmail.com>
2020-02-07 21:21:51 -0300

    Alteração do Retorno da API - V1.0.1

--- routes/router.js
+++ routes/router.js
@@ -9,7 +9,15 @@ const controllers = require('../controllers');
 
 /* GET information of repository listing. */
 router.get('/repositories', async function(req, res, next) {
-    res.send((JSON.stringify(await controllers.repositoryOrdenation("takenet"))));
+    //res.send((JSON.stringify(await controllers.repositoryOrdenation("takenet"))));
+    const repo = await controllers.repositoryOrdenation("takenet");
+    res.send({
+        1: repo[0],
+        2: repo[1],
+        3: repo[2],
+        4: repo[3],
+        5: repo[4]
+    });
 });
 
 module.exports = router;
